b6f46604ae8699d90131a23c1bae32a10a4d9954
Elvira-xin <840137893@qq.com>
2019-10-22 18:14:57 +0800

    登录页结构完成

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,108 @@
 <template>
   <div class="login">
-      这是登录页
+    <!-- <div class="block">
+      <el-carousel trigger="click" >
+        <el-carousel-item v-for="item in 4" :key="item">
+          <h3>{{ item }}</h3>
+        </el-carousel-item>
+      </el-carousel>
+    </div>-->
+    <!-- 登录框 -->
+    <div class="login-title">登录</div>
+    <el-form
+      :model="ruleForm"
+      :rules="rules"
+      ref="ruleForm"
+      label-width="70px"
+      class="demo-ruleForm"
+    >
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model="ruleForm.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input v-model="ruleForm.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      ruleForm: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入正确的用户名', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入正确的密码', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    submitForm (formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    resetForm (formName) {
+      this.$refs[formName].resetFields()
+    }
+  }
 }
 </script>
 
-<style lang="less" scoped>
-// .login{
-//     font-size: 30px;
-//     color: red
-// }
+ <style lang="less" scoped>
+//  .el-carousel{
+//      height: 100%;
+//  }
+//   .el-carousel__item h3 {
+//     color: #475669;
+//     font-size: 14px;
+//     opacity: 0.75;
+//     line-height: 150px;
+//     margin: 0;
+//   }
+//   .el-carousel__item:nth-child(2n) {
+//      background-color: #99a9bf;
+//   }
+
+//   .el-carousel__item:nth-child(2n+1) {
+//      background-color: #d3dce6;
+//   }
+.login {
+  background-color: #99a9bf;
+  position: fixed;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.demo-ruleForm {
+  background-color: pink;
+  padding: 40px;
+  border-radius: 5%
+}
+.login-title {
+  color: white;
+}
+.el-form-item__label {
+  color: white;
+}
 </style>
