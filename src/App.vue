1d206351f4dacff50190dd8b8d744f5d10e1e75b
Elvira-xin <840137893@qq.com>
2019-10-22 17:32:55 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <!-- 根组件 -->
+  <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login">
+      这是登录页
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+// .login{
+//     font-size: 30px;
+//     color: red
+// }
+</style>
