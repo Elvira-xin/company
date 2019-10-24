0dfbe2cb0781da19e4351ca77222992b3934e058
周欣 <840137893@qq.com>
2019-10-24 17:02:56 +0800

    登录页初稿

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,32 +1,33 @@
 <template>
   <div class="login">
     <!-- 登录框 -->
-    <div class="container">
-      <!-- tab栏 -->
-      <div class="loginForm-tab">
-        <span class="login-account">摩富金服后台管理系统</span>
+    <div class="loginForm">
+      <div class="loginForm-top">
+        后台管理系统
+        <span class="bg1"></span>
+        <span class="bg2"></span>
       </div>
       <el-form
         :model="ruleForm"
         :rules="rules"
         ref="ruleForm"
-        label-width="70px"
+
         class="demo-ruleForm"
       >
         <!-- 输入框 -->
-
-        <el-form-item label="用户名" prop="username">
-          <el-input v-model="ruleForm.username" prefix-icon="myicon-user"></el-input>
+        <el-form-item prop="username">
+          <el-input v-model="ruleForm.username" placeholder="请输入账号" prefix-icon="myicon-user" class="loginForm-input"></el-input>
         </el-form-item>
-        <el-form-item label="密码" prop="password">
-          <el-input v-model="ruleForm.password" prefix-icon="myicon-key"></el-input>
+        <el-form-item  prop="password">
+          <el-input v-model="ruleForm.password" placeholder="请输入密码" prefix-icon="myicon-key" class="loginForm-input"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button class="login-btn" type="primary" plain @click="submitForm('ruleForm')">登录</el-button>
+          <el-button class="login-btn" type="warning" plain @click="submitForm('ruleForm')">立即登录</el-button>
         </el-form-item>
       </el-form>
+       <el-checkbox v-model="checked" class="login-auto">两周内自动登录</el-checkbox>
       <span class="findPassword">
-        <a href="#">找回密码</a>
+        <a href="#">忘记密码？</a>
       </span>
     </div>
   </div>
@@ -51,63 +52,89 @@ export default {
     }
   },
   methods: {
-    submitForm (formName) {
-      this.$refs[formName].validate(valid => {
-        if (valid) {
-          alert('submit!')
-        } else {
-          console.log('error submit!!')
-          return false
-        }
-      })
-    },
-    resetForm (formName) {
-      this.$refs[formName].resetFields()
-    }
+    submitForm () {}
   }
 }
 </script>
 
- <style lang="less" scoped>
+<style lang="less" scoped>
 .login {
   position: fixed;
-  width: 2000px;
-  height: 1000px;
+  width: 100%;
+  height: 90%;
+    top: 5%;
   background-color: #2f4050;
-  background: url("../img/311993150.jpg") no-repeat;
+  background: url("../img/308296275.jpg") no-repeat;
   background-size: cover;
-  .container {
+  .loginForm {
     position: absolute;
     left: 0;
     right: 0;
-    bottom: 182px;
-    width: 420px;
-    height: 300px;
+    bottom: -48px;
+    width: 408px;
+    height: 428px;
     margin: 200px auto;
-    padding: 30px;
     background: white;
-    border: 7px solid #eee;
-    border-radius: 30px;
-    .loginForm-tab {
+    border-radius: 12px;
+    .loginForm-top {
+      border-radius: 12px 12px 0 0;
       width: 100%;
-      line-height: 70px;
+      height: 87px;
+      background-color: #db7e0c;
+      opacity: .85;
+      //   color: #bd3633;
+      color: #fff;
+      font-size:34px;
+      font-weight: 400;
+      line-height: 87px;
       text-align: center;
-      font-size: 24px;
       margin-bottom: 25px;
-      .login-account {
-        color: #4b6796;
+      position: relative;
+      .bg1 {
+        background-color: #eee;
+        top: -38px;
+        right: -16px;
+        opacity: 0.1;
+        position: absolute;
+        display: inline-block;
+        width: 94px;
+        height: 94px;
+        border-radius: 50%;
+      }
+      .bg2 {
+        background-color: #eee;
+        width: 94px;
+        height: 94px;
+        position: absolute;
+        opacity: 0.1;
+        border-radius: 50%;
+        top:35px;
+        left: -22px;
       }
     }
     .demo-ruleForm {
-      padding: 0px 40px 15px 30px;
+      padding: 20px 40px 15px 30px;
+    }
+    /deep/ .loginForm-input .el-input__inner {
+        border: 0 none;
+        border-bottom: 1px solid #ccc;
+        border-radius: 0px;
+
     }
     .login-btn {
       width: 100%;
+    border-radius: 16px;
+    }
+    .login-auto{
+        position: absolute;
+        bottom: 70px;
+        left: 60px;
+        color: #9abcda;
     }
     .findPassword {
       position: absolute;
       right: 60px;
-      bottom: 20px;
+      bottom: 73px;
       a {
         color: #4b6796;
         font-size: 12px;
